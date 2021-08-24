88045c31759e77ea519ee9a58080a124fc905e73
Brandon Myers <brandonRmyers1512@gmail.com>
2021-08-24 16:46:02 -0700

    updating links

--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -2,7 +2,7 @@ var $videoPlayer = $("#videoPlayer");
 var $videoTitle = $("#videoTitle");
 
 const videoTitles = ["Video 1", "Video 2", "Video 3"];
-const videoSources = ["../videos/video1.mp4"," ../videos/video2.mp4", "../videos/video3.mp4"];
+const videoSources = ["./videos/video1.mp4"," ./videos/video2.mp4", "../videos/video3.mp4"];
 // the current position the user is in the video array
 var videoPos = 0;
 
